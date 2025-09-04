92add2f55c0d3f5632b6234fd1c51e31159c5555
obagriacik-coder <o.bagriacik@gmail.com>
2025-09-04 15:05:34 +0300

    Update MainActivity.kt

--- app/src/main/java/com/pisyedili/game/MainActivity.kt
+++ app/src/main/java/com/pisyedili/game/MainActivity.kt
@@ -6,20 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface(color = MaterialTheme.colorScheme.background) {
-                Greeting("Pis Yedili")
+            MaterialTheme {
+                Surface {
+                    Text("PisYedili açıldı!")
+                }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Merhaba $name!")
 }
